ce480d92c44646fda4fefd586481816f6ce0e21e
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-10-17 08:11:48 +0530

    Tree Program(inOrder,preOrder & postOrder)

---
+++ DSA C & C++/DSA/Graph & Tree/Tree/tree.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int value) : data(value), left(nullptr), right(nullptr) {}
+};
+
+void inOrderTraversal(Node *root)
+{
+    if (!root)
+        return;
+    inOrderTraversal(root->left);
+    cout << root->data << " ";
+    inOrderTraversal(root->right);
+}
+
+void preOrderTraversal(Node *root)
+{
+    if (!root)
+        return;
+    cout << root->data << " ";
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+}
+
+void postOrderTraversal(Node *root)
+{
+    if (!root)
+        return;
+    postOrderTraversal(root->left);
+    postOrderTraversal(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout << "Inorder Traversal: ";
+    inOrderTraversal(root);
+    cout << "\n";
+
+    cout << "Preorder Traversal: ";
+    preOrderTraversal(root);
+    cout << "\n";
+
+    cout << "Postorder Traversal: ";
+    postOrderTraversal(root);
+    cout << "\n";
+
+    return 0;
+}
