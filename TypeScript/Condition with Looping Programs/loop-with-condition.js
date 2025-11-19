c17c2c79ff925627ffd045cdadc7b3ae768c0099
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-11-19 15:43:27 +0530

    Condition with Looping Program

---
+++ TypeScript/Condition with Looping Programs/loop-with-condition.js
@@ -0,0 +1,9 @@
+// ODD-EVEN using Condition with Looping 
+for (var i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log("".concat(i, " is Even"));
+    }
+    else {
+        console.log("".concat(i, " is Odd"));
+    }
+}
---
+++ TypeScript/Condition with Looping Programs/loop-with-condition.ts
@@ -0,0 +1,9 @@
+// ODD-EVEN using Condition with Looping 
+
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} is Even`);
+    } else {
+        console.log(`${i} is Odd`);
+    }
+}
