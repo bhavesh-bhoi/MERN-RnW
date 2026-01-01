f47938e77f40ce7461dfad9f8f298afc3e919810
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-01-01 21:13:53 +0530

    Revision:Solving Practice Questions For Valid/Invalid Variable Names

--- Javascript/Revision/script.js
+++ Javascript/Revision/script.js
@@ -1,4 +1,12 @@
 // 1) What is the value of each variable in each line of the following code?
-let num = 5;
-let newNum = num++;
-newNum = ++num;
+let num = 5;                // num = 5 - initializes num with the value 5
+let newNum = num++;          //  newNum = 5, num = 6 - stores the current value of num, then increments num by 1
+newNum = ++num;          // newNum = 7, num = 7 - increments num by 1, then stores the new value of num in newNum
+
+// 2) Find Errors in following code
+let 1age = 16; // Error: Variable names cannot start with a digit
+
+let marks = 45;
+let isMarks = True; // Error: 'True' should be 'true' (lowercase)
+
+let isPass = 'true'; // Error: Boolean values should not be in quotes
