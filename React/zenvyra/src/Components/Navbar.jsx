6d485012e821faa8eb19ba595f32879e363322a2
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-01-10 14:19:57 +0530

    Updated:Buttons in Navbar

--- React/zenvyra/src/Components/Navbar.jsx
+++ React/zenvyra/src/Components/Navbar.jsx
@@ -2,43 +2,44 @@ function Navbar() {
   return (
     <div className="w-full p-6 bg-black text-white flex items-center justify-between">
       <div className="text-4xl font-bold">
-        <h1 className="font-pacifico">Zenvyra</h1>
+        <h1 className="hero-title">Zenvyra</h1>
       </div>
       <div className="text-lg items-center">
         <ul className="flex space-x-6">
           <li>
-            <a href="#" className="hover:text-amber-100">
+            <a href="#" className="hover:text-amber-200">
               Home
             </a>
           </li>
           <li>
-            <a href="#" className="hover:text-amber-100">
+            <a href="#" className="hover:text-amber-200">
               About
             </a>
           </li>
           <li>
-            <a href="#" className="hover:text-amber-100">
+            <a href="#" className="hover:text-amber-200">
               Pricing
             </a>
           </li>
           <li>
-            <a href="#" className="hover:text-amber-100">
+            <a href="#" className="hover:text-amber-200">
               FAQs
             </a>
           </li>
           <li>
-            <a href="#" className="hover:text-amber-100">
+            <a href="#" className="hover:text-amber-200">
               Contact
             </a>
           </li>
         </ul>
       </div>
       <div className="space-x-4">
-        <button className="bg-amber-500 text-black px-4 py-2 rounded hover:bg-amber-600 hover:cursor-pointer">
-          Login
+        <button className="btn-login">
+          <span>Login</span>
         </button>
-        <button className="bg-amber-500 text-black px-4 py-2 rounded hover:bg-amber-600 hover:cursor-pointer">
-          Get Started
+
+        <button className="btn-signup">
+          <span>Get Started</span>
         </button>
       </div>
     </div>
