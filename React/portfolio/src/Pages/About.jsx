d04c49bdc5055646b4c47bef078ec0e67da34910
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-01-16 15:03:49 +0530

    Added:Designing About Page

--- React/portfolio/src/Pages/About.jsx
+++ React/portfolio/src/Pages/About.jsx
@@ -1,7 +1,11 @@
 function About() {
   return (
-    <div className="flex-1 ml-73.75 h-screen bg-black bg-center bg-no-repeat relative flex items-center select-none">
-        
+    <div className="flex-1 ml-73.75 h-screen bg-black select-none p-4">
+      <div className="p-5">
+        <h1 className="text-white font-bold text-3xl pt-16">About</h1>
+        <hr className="bg-blue-300 w-22 pt-1 mt-1 rounded-full" />
+        <p className="text-md text-white mt-15">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate tempore earum sit exercitationem ducimus eum, iusto at neque magnam a asperiores soluta ullam consequatur! Ipsum in praesentium exercitationem molestias sit.</p>
+      </div>
     </div>
   );
 }
